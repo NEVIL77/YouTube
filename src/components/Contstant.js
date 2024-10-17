ab633d293e933453636e710c6745de4068f21ce2
NEVIL77 <patelnevil778@gmail.com>
2024-10-18 00:58:04 +0530

    header css

--- src/components/Contstant.js
+++ src/components/Contstant.js
@@ -1,5 +1,6 @@
 export const  constant = {
     HAMBERGERICON : 'https://imgs.search.brave.com/mEqmMKnNI-Ni-kMXybHKv3Pv_s5_F1Ti8cAujIrS2t8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA2Lzc2LzE1LzE1/LzM2MF9GXzY3NjE1/MTU5OF9venJKSlNF/QzZkMXZPZGVFVzVY/MVBka3FSSjRRclBn/by5qcGc' ,
     YOUTUBE : 'https://t3.ftcdn.net/jpg/05/48/51/54/360_F_548515477_Xzfcwp5fl1ffhbc5sO3kHkwrLHQQUmM9.jpg',
-    USERICON : 'https://cdn1.iconfinder.com/data/icons/chunk/16/User-512.png'
+    USERICON : 'https://cdn1.iconfinder.com/data/icons/chunk/16/User-512.png',
+    SEARCHICON : 'https://t4.ftcdn.net/jpg/05/58/27/27/360_F_558272798_DNqj4q2TXE7EsDM9Zp2wdyap8gzatwlF.jpg'
 }
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import {constant} from './Contstant'
+import React from 'react';
+import { constant } from './Contstant';
 
 const Head = () => {
   return (
-    <div className='grid grid-flow-col p-5 m-2 shadow-lg w-full'>
-        <div className='flex col-span-1'>
-          <img 
-            className='h-10'
-            src={constant?.HAMBERGERICON} alt='menu'/>
-          <img 
-            className='h-10 mx-2'
-            src={constant?.YOUTUBE} alt='youTube'/>
-        </div>
-        <div className='col-span-10 px-10'>
-          <input 
-            className='h-10 w-1/2 bg-slate-100 px-5'
-            type='text'placeholder='Search here...'/>
-          <button>Search</button>
-        </div>
-        <div>
-          <img 
-            className='h-10'
-            src={constant?.USERICON} alt='search' />
-        </div>
-    </div>
-  )
-}
+    <div className='grid grid-flow-col items-center p-4 m-2 shadow-lg w-full'>
+      {/* Left section with icons */}
+      <div className='flex items-center col-span-1'>
+        <img className='h-10' src={constant?.HAMBERGERICON} alt='menu' />
+        <img className='h-10 mx-3' src={constant?.YOUTUBE} alt='YouTube' />
+      </div>
 
-export default Head
+      {/* Center search bar */}
+      <div className='col-span-10 flex justify-center'>
+        <input 
+          className='h-10 w-1/2 bg-slate-100 border border-gray-300 px-5 rounded-l-full focus:outline-none' 
+          type='text' 
+          placeholder='Search here...' 
+        />
+        <button className='h-10 w-16 bg-white border border-gray-300 shadow-md rounded-r-full'>
+          <img className='h-6 w-6 mx-auto' src={constant?.SEARCHICON} alt='search' />
+        </button>
+      </div>
+
+      {/* Right section with user icon */}
+      <div className='flex justify-end items-center'>
+        <img className='h-10' src={constant?.USERICON} alt='user' />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
